8898ac69523a83629eda3b369d5d3ff7bc361e7c
SrikanthMalla <malla.srikanth2012@vit.ac.in>
2017-04-20 13:16:48 -0400

    figures saved

--- svm_irl.m
+++ svm_irl.m
@@ -12,40 +12,40 @@ mu_e=expectation_features(start_state,macrocell,m)
 
 
 %% algo starts here
-m1=mdp;m1.grid_size=grid_size;
-policy={m.random_policy()};
-m1.policy=policy{1};
-% traj=m1.generate_trajectory(start_state,length)
-mu=expectation_features(start_state,macrocell,m1);
-t=inf;
-n=size(mu,1);m=size(mu,2);
-epochs=0;
-w_list=[];
-
-while (t>exp(-9))
-    epochs=epochs+1
-    cvx_begin
-        variable w(n,1)
-        variable t
-        maximise(t)
-        subject to
-            norm(w,2)<=1
-            w'*(mu-mu_e*ones(1,m))<=-t.*ones(1,m)
-    cvx_end
-    w=normc(w);
-    set_reward(w,macrocell,m1)
-    m1.new_states=set_reward(w,macrocell,m1);
-    m1=m1.value_iter();
-    m1=m1.get_optimal_policy;
-    % traj=m1.generate_trajectory(start_state,length)
-    mu=[mu,expectation_features(start_state,macrocell,m1)];
-    n=size(mu,1);m=size(mu,2);
-    m1.grid_plotter(m1.grid_size,m1.states./max(m1.states(:)));
-    w_list=([w_list,w])   
-end
-mu_e
-mu
-t
+% m1=mdp;m1.grid_size=grid_size;
+% policy={m.random_policy()};
+% m1.policy=policy{1};
+% % traj=m1.generate_trajectory(start_state,length)
+% mu=expectation_features(start_state,macrocell,m1);
+% t=inf;
+% n=size(mu,1);m=size(mu,2);
+% epochs=0;
+% w_list=[];
+% 
+% while (t>exp(-9))
+%     epochs=epochs+1
+%     cvx_begin
+%         variable w(n,1)
+%         variable t
+%         maximise(t)
+%         subject to
+%             norm(w,2)<=1
+%             w'*(mu-mu_e*ones(1,m))<=-t.*ones(1,m)
+%     cvx_end
+%     w=normc(w);
+%     set_reward(w,macrocell,m1)
+%     m1.new_states=set_reward(w,macrocell,m1);
+%     m1=m1.value_iter();
+%     m1=m1.get_optimal_policy;
+%     % traj=m1.generate_trajectory(start_state,length)
+%     mu=[mu,expectation_features(start_state,macrocell,m1)];
+%     n=size(mu,1);m=size(mu,2);
+%     m1.grid_plotter(m1.grid_size,m1.states./max(m1.states(:)));
+%     w_list=([w_list,w])   
+% end
+% mu_e
+% mu
+% t
 end
 function states=set_reward(w,macrocell,m1)
 states=zeros(size(m1.states));
